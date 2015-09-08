3631cd8c915767788d6c6e48539f2c84b0dc2e2d
GabiOC <oconnor.gabriele@gmail.com>
2015-09-08 18:12:59 -0400

    all done

--- js/bike.js
+++ js/bike.js
@@ -1,7 +1,23 @@
-function Bike(){
+function Bike(name, type, color){
   // attributes go here...
+  this.name = name;
+  this.bikeType = type;
+  this.frameColor = color;
+  this.condition = "ready to go!";
+  this.WHEELS = 2;
 }
 
-Bike.prototype.someInstanceMethod = function(){
+Bike.prototype.takeForARide = function(miles){
   // code goes here...
+  if (miles > 50){
+    this.condition = 'needs a tune up';
+  }
+  else {
+    'hello';
+  }
+};
+
+Bike.prototype.tuneUp = function(){
+  // code goes here...
+  this.condition = 'ready to go!';
 };
